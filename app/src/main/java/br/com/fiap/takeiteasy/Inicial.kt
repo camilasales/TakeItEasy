93a525b7452888bc3bfc8354b6b5060edf3868e2
Camila Sales <camila.07ramones@gmail.com>
2022-12-03 00:57:59 -0300

    feat: adicona api de valores nutricionais

--- app/src/main/java/br/com/fiap/takeiteasy/Inicial.kt
+++ app/src/main/java/br/com/fiap/takeiteasy/Inicial.kt
@@ -1,11 +1,25 @@
 package br.com.fiap.takeiteasy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Inicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicial)
+
+        val btnDesafio1: Button = findViewById(R.id.btn_desafio1);
+
+        btnDesafio1.setOnClickListener(){
+            openActivityMinhaSaude()
+        }
+
+    }
+
+    fun openActivityMinhaSaude(){
+        val intent = Intent(this, MinhaSaude::class.java);
+        startActivity(intent);
     }
 }
--- app/src/main/java/br/com/fiap/takeiteasy/MinhaSaude.kt
+++ app/src/main/java/br/com/fiap/takeiteasy/MinhaSaude.kt
@@ -2,10 +2,39 @@ package br.com.fiap.takeiteasy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MinhaSaude : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_minha_saude)
+
+        val layoutFruta: LinearLayout = findViewById(R.id.layout_fruta)
+
+        layoutFruta.setOnClickListener {
+            val call = RetrofitFactory().retrofitServicFruit().getAliments();
+
+
+            call.enqueue(object : Callback<ALIMENTO> {
+                override fun onResponse(call: Call<ALIMENTO>, response: Response<ALIMENTO>) {
+                    response.body()?.let {
+                        Log.i("CEP", it.toString())
+
+                        Toast.makeText(this@MinhaSaude, it.toString(), Toast.LENGTH_LONG).show()
+
+                    } ?: Toast.makeText(this@MinhaSaude, "CEP não localizado", Toast.LENGTH_LONG)
+                        .show()
+                }
+
+                override fun onFailure(call: Call<ALIMENTO>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            });
+        }
     }
 }
